60f07c9ab38fe389a56bfa1f9b129965a080657f
Tausifqureshi786 <59664677+Tausifqureshi786@users.noreply.github.com>
2022-01-28 15:59:30 -0700

    connect with mongodb and custom error handling

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require("express");
-
 const app = express();
-const tasks = require('./routes/tasks')
- 
+const tasks = require('./routes/tasksRouter')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errHandlerMiddleware = require('./middleware/error-handler')
+
+//middleware
+// app.use(express.static()) for serving static files
+app.use(express.json());
+
+// routes
 app.get('/', (req,res) =>{
   res.send('Hello from NODE JS');
 })
 
 app.use('/api/v1/tasks', tasks)
 
-app.listen(4000, console.log(`server listening on port 4000`));
+app.use(notFound)
+app.use(errHandlerMiddleware)
+
+const port = process.env.PORT ||4000
+const start = async () => {
+  try{
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`server listening on port ${port}`));
+  } catch(error){
+    console.log(error)
+  } 
+}
+
+start();
